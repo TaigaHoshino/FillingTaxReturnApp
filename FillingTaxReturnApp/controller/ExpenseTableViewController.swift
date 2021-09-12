5ff4c33650c4997f0a4cd48063fb140ab01f04e8
tanukis <tiger3rdstar@gmail.com>
2021-09-13 07:05:39 +0900

    TransactionListViewControllerからリストビューの部分を入れ替え可能にした

---
+++ FillingTaxReturnApp/controller/ExpenseTableViewController.swift
@@ -0,0 +1,171 @@
+//
+//  ExpenseTableViewController.swift
+//  FillingTaxReturnApp
+//
+//  Created by 星野大我 on 2021/09/12.
+//
+
+import UIKit
+import CoreData
+
+class ExpenseTableViewController: UITableViewController {
+    
+    private var date: Date!
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Expense> = {
+        let sortDescripter = NSSortDescriptor(key: "occuredAt", ascending: true)
+        
+        let result = getMonthRange(date: date)
+        let from = result["from"]!
+        let to = result["to"]!
+        
+        let predicate = NSPredicate(format: "(occuredAt >= %@) AND (occuredAt < %@) AND (isRegistered <> 'false')", from as CVarArg, to as CVarArg)
+        let controller: NSFetchedResultsController<Expense> = BaseDataModel.getFetchedResultController(sortDescriptors: [sortDescripter], predicate: predicate, sectionNameKeyPath: "occuredAt")
+        controller.delegate = self
+        
+        return controller
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        do {
+            try fetchedResultsController.performFetch()
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    public func setDate(date: Date) {
+        
+        self.date = date
+        
+        let result = getMonthRange(date: date)
+        let from = result["from"]!
+        let to = result["to"]!
+        
+        do {
+            fetchedResultsController.fetchRequest.predicate = NSPredicate(format: "(occuredAt >= %@) AND (occuredAt < %@) AND (isRegistered <> 'false')", from as CVarArg, to as CVarArg)
+            try fetchedResultsController.performFetch()
+        }
+        catch {
+            print(error)
+        }
+        tableView.reloadData()
+    }
+    
+    private func getMonthRange(date: Date) -> [String: Date]{
+        let year = date.year
+        let month = date.month
+        
+        let from = DatetimeUtil.formattedDateToDate(strDate: "\(year)年\(month)月1日")
+        var to: Date
+        
+        if month != 12 {
+            to = DatetimeUtil.formattedDateToDate(strDate: "\(year)年\(month + 1)月1日")
+        }
+        else{
+            to = DatetimeUtil.formattedDateToDate(strDate: "\(year + 1)年1月1日")
+        }
+        
+        return ["from": from, "to": to]
+        
+    }
+    
+    public static func getInitialController(date: Date) -> Self {
+        let storyboard = UIStoryboard(name: "ExpenseTable", bundle: nil)
+        let viewController = storyboard.instantiateViewController(identifier: "ExpenseTableViewController") as! Self
+        viewController.date = date
+        return viewController
+    }
+}
+
+extension ExpenseTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard let sections = fetchedResultsController.sections else {return 0}
+        
+        let sectionInfo = sections[section]
+        
+        return sectionInfo.numberOfObjects
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        guard let sections = fetchedResultsController.sections else {return ""}
+        
+        let sectionName = sections[section].name
+        let charloc = sectionName[...sectionName.range(of: " ")!.lowerBound]
+        
+        let returnValue = String(charloc)
+        
+        return returnValue
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionCell", for: indexPath)
+        
+        
+        if let cell = cell as? TransactionCell {
+            let expense = fetchedResultsController.object(at: indexPath)
+            cell.setupCell(expense: expense)
+        }
+        
+        return cell
+    }
+    
+}
+
+
+extension ExpenseTableViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if let cell = cell as? TransactionCell {
+            
+            let detailedReceitViewController = DetailedExpenseViewController.getInitialViewController(expense: cell.getExpense())
+            present(detailedReceitViewController, animated: true, completion: nil)
+        }
+    }
+}
+
+extension ExpenseTableViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: 80, height: 50)
+    }
+    
+}
+
+extension ExpenseTableViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [indexPath!], with: .automatic)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .automatic)
+        case .move:
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .automatic)
+        @unknown default:
+            break;
+        }
+        
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+}
--- FillingTaxReturnApp/controller/TransactionListViewController.swift
+++ FillingTaxReturnApp/controller/TransactionListViewController.swift
@@ -11,73 +11,67 @@ import CoreData
 class TransactionListViewController: UIViewController {
     
     @IBOutlet weak var dateCollectionView: UICollectionView!
-    
-    @IBOutlet weak var tvTransactionList: UITableView!
+    @IBOutlet weak var containerView: UIView!
     
     private var selectedDateCellIndexPath: IndexPath!
     private var currentSelectedDate = Date()
+    private var expenseTableView: ExpenseTableViewController!
     
-    lazy var fetchedResultsController: NSFetchedResultsController<Expense> = {
-        let sortDescripter = NSSortDescriptor(key: "occuredAt", ascending: true)
-        
-        let result = getMonthRange(date: currentSelectedDate)
-        let from = result["from"]!
-        let to = result["to"]!
-        
-        let predicate = NSPredicate(format: "(occuredAt >= %@) AND (occuredAt < %@) AND (isRegistered <> 'false')", from as CVarArg, to as CVarArg)
-        let controller: NSFetchedResultsController<Expense> = BaseDataModel.getFetchedResultController(sortDescriptors: [sortDescripter], predicate: predicate, sectionNameKeyPath: "occuredAt")
-        controller.delegate = self
-        
-        return controller
-    }()
+    var contentViewController: UIViewController? {
+        didSet {
+          // 問題1の対応
+          // セットされていた子を削除
+          oldValue?.willMove(toParent: nil)
+          oldValue?.view.removeFromSuperview()
+          oldValue?.removeFromParent()
+
+          // 問題2の対応 - 1
+          // viewがロードされていない場合、子は追加するが子のviewの追加は行わない
+            addChild(contentViewController!)  // (b)
+            contentViewController!.didMove(toParent: self)
+
+          guard isViewLoaded else {
+            return
+          }
+          setupContentView()
+        }
+    }
+    
+    private func setupContentView() {
+        guard let contentViewController = contentViewController else {
+          return
+        }
+        let isAlreadyAdded = (contentViewController.isViewLoaded) && containerView.subviews.contains(contentViewController.view)
+        guard isAlreadyAdded == false else {
+          return // 二重呼び出し防止
+        }
+
+        contentViewController.view.frame = containerView.bounds
+        containerView.addSubview(contentViewController.view)  // (c)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dateCollectionView.dataSource = self
         dateCollectionView.delegate = self
-        tvTransactionList.dataSource = self
-        tvTransactionList.delegate = self
         
         dateCollectionView.contentInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
         
         currentSelectedDate = Date()
-
-        // Do any additional setup after loading the view.
+        
+        expenseTableView = ExpenseTableViewController.getInitialController(date: currentSelectedDate)
+        contentViewController = expenseTableView
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
-        do {
-            try fetchedResultsController.performFetch()
-        }
-        catch {
-            print(error)
-        }
         
         let month = currentSelectedDate.month
         
         // 今日の月のコレクションセルを探し、中央に表示して選択状態にする
         selectedDateCellIndexPath = IndexPath(row: month - 1, section: 0)
         dateCollectionView.scrollToItem(at: selectedDateCellIndexPath, at: .centeredHorizontally, animated: false)
-        
-    }
-    
-    private func getMonthRange(date: Date) -> [String: Date]{
-        let year = currentSelectedDate.year
-        let month = currentSelectedDate.month
-        
-        let from = DatetimeUtil.formattedDateToDate(strDate: "\(year)年\(month)月1日")
-        var to: Date
-        
-        if month != 12 {
-            to = DatetimeUtil.formattedDateToDate(strDate: "\(year)年\(month + 1)月1日")
-        }
-        else{
-            to = DatetimeUtil.formattedDateToDate(strDate: "\(year + 1)年1月1日")
-        }
-        
-        return ["from": from, "to": to]
         
     }
 
@@ -121,71 +115,8 @@ extension TransactionListViewController: UICollectionViewDataSource {
             cell.onSelect()
             selectedDateCellIndexPath = indexPath
             currentSelectedDate = cell.getDate()
-            let result = getMonthRange(date: currentSelectedDate)
-            let from = result["from"]!
-            let to = result["to"]!
-            do {
-                fetchedResultsController.fetchRequest.predicate = NSPredicate(format: "(occuredAt >= %@) AND (occuredAt < %@) AND (isRegistered <> 'false')", from as CVarArg, to as CVarArg)
-                try fetchedResultsController.performFetch()
-            }
-            catch {
-                print(error)
-            }
-            tvTransactionList.reloadData()
-        }
-    }
-}
-
-extension TransactionListViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return fetchedResultsController.sections?.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        guard let sections = fetchedResultsController.sections else {return 0}
-        
-        let sectionInfo = sections[section]
-        
-        return sectionInfo.numberOfObjects
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
-        guard let sections = fetchedResultsController.sections else {return ""}
-        
-        let sectionName = sections[section].name
-        let charloc = sectionName[...sectionName.range(of: " ")!.lowerBound]
-        
-        let returnValue = String(charloc)
-        
-        return returnValue
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tvTransactionList.dequeueReusableCell(withIdentifier: "TransactionCell", for: indexPath)
-        
-        
-        if let cell = cell as? TransactionCell {
-            let expense = fetchedResultsController.object(at: indexPath)
-            cell.setupCell(expense: expense)
-        }
-        
-        return cell
-    }
-    
-}
-
-extension TransactionListViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tvTransactionList.cellForRow(at: indexPath)
-        
-        if let cell = cell as? TransactionCell {
+            expenseTableView.setDate(date: currentSelectedDate)
             
-            let detailedReceitViewController = DetailedExpenseViewController.getInitialViewController(expense: cell.getExpense())
-            present(detailedReceitViewController, animated: true, completion: nil)
         }
     }
 }
@@ -196,30 +127,4 @@ extension TransactionListViewController: UICollectionViewDelegateFlowLayout {
         return CGSize.init(width: 80, height: 50)
     }
     
-}
-
-extension TransactionListViewController: NSFetchedResultsControllerDelegate {
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            tvTransactionList.insertRows(at: [indexPath!], with: .automatic)
-        case .delete:
-            tvTransactionList.deleteRows(at: [indexPath!], with: .automatic)
-        case .move:
-            tvTransactionList.moveRow(at: indexPath!, to: newIndexPath!)
-        case .update:
-            tvTransactionList.reloadRows(at: [indexPath!], with: .automatic)
-        @unknown default:
-            break;
-        }
-        
-    }
-    
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tvTransactionList.beginUpdates()
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tvTransactionList.endUpdates()
-    }
 }
